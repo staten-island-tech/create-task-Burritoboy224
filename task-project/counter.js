588c02d7227277d033dfd9728aac143f24f6cb8d
Yuhan224 <fir3panic@gmail.com>
2024-01-17 09:23:41 -0500

    f

---
+++ task-project/counter.js
@@ -0,0 +1,31 @@
+const millionaireQuestions = [
+    {
+      question: "What is the capital city of France?",
+      options: ["London", "Paris", "Berlin", "Rome"],
+      answer: 1
+    },
+    {
+      question: "Who painted the Mona Lisa?",
+      options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Salvador Dalí"],
+      answer: 2
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Mars", "Venus", "Jupiter", "Saturn"],
+      answer: 0
+    },
+    {
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      options: ["William Shakespeare", "Jane Austen", "Mark Twain", "George Orwell"],
+      answer: 0
+    }
+    {
+      question: "What is the chemical symbol for gold?",
+      options: ["Au", "Go", "Gu", "Fa"],
+      answer: 0
+    }
+    {
+      
+    }
+  ];
+  
