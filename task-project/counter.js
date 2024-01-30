4b8669ae549ad7759b6b02e741ace62e248a395f
Yuhan224 <fir3panic@gmail.com>
2024-01-30 09:20:09 -0500

    k

--- task-project/counter.js
+++ task-project/counter.js
@@ -1,38 +1,36 @@
-const Questions = [
-    {
-      question: "What is the capital city of France?",
-      options: ["London", "Paris", "Berlin", "Rome"],
-      answer: 1
-    },
-    {
-      question: "Who painted the Mona Lisa?",
-      options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Salvador Dalí"],
-      answer: 2
-    },
-    {
-      question: "Which planet is known as the Red Planet?",
-      options: ["Mars", "Venus", "Jupiter", "Saturn"],
-      answer: 0
-    },
-    {
-      question: "Who wrote the play 'Romeo and Juliet'?",
-      options: ["William Shakespeare", "Jane Austen", "Mark Twain", "George Orwell"],
-      answer: 0
-    },
-    {
-      question: "What is the chemical symbol for gold?",
-      options: ["Au", "Go", "Gu", "Fa"],
-      answer: 0
-    },
-    {
-      question: "Which Disney character famously leaves a glass slipper behind at a royal ball?",
-      options: ["Pocahontas", "Sleeping Beauty", "Cinderella", "Elsa"],
-      answer: 0
-    } ,
-     {
-      question: "The hammer and sickle is one of the most recognisable symbols of which political ideology?",
-      options: ["Republicanism", "Communism", "Conservatism", "Liberalism"],
-      answer: 0
-     },
-  ];
-export {counter};
+const questions = [
+  {
+    question: "What is the capital of France?",
+    answers: ["Paris", "London", "Berlin", "Kieran"],
+    correctAnswer: "Paris"
+  },
+  {
+    question: "Who wrote the Harry Potter book series?",
+    answers: ["J.K. Rowling", "Dr. Seuss", "Mark Twain", "Jayla Ye"],
+    correctAnswer: "J.K. Rowling"
+  },
+  {
+    question: "Which planet is closest to the sun?", 
+    answers: ["Earth", "Venus", "Mercury", "Mars"],
+    correctAnswer: "Mercury"
+  },
+  {
+    question: "A magnet would most likely attract which of the following?", 
+    answers: ["Metal", "Plastic", "Burrito", "Mars"],
+    correctAnswer: "Metal"
+  },
+  {
+    question: "Which planet is closest to Earth?", 
+    answers: ["Earth", "Venus", "Mercury", "Mars"],
+    correctAnswer: "Mercury"
+  },
+  {
+    question: "Who made the iphone", 
+    answers: ["Steve Job", "Jayla Ye", "Steve Trabajo", "Kieran Watt"],
+    correctAnswer: "Steve Job"
+  },
+  
+  
+];
+
+export {questions};
