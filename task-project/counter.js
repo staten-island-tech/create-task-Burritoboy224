99b185c5c2759c90464d96607576308dd5364f25
Yuhan224 <fir3panic@gmail.com>
2024-02-01 09:21:15 -0500

    uh

--- task-project/counter.js
+++ task-project/counter.js
@@ -1,60 +1,119 @@
 const questions = [
   {
-    question: "What is the capital of France?",
+    question: "1) What is the capital of France?",
     answers: ["Paris", "London", "Berlin", "Kieran"],
     correctAnswer: "Paris"
   },
   {
-    question: "Who wrote the Harry Potter book series?",
+    question: "2) Who wrote the Harry Potter book series?",
     answers: ["J.K. Rowling", "Dr. Seuss", "Mark Twain", "Jayla Ye"],
     correctAnswer: "J.K. Rowling"
   },
   {
-    question: "Which planet is closest to the sun?", 
+    question: "3) Which planet is closest to the sun?", 
     answers: ["Earth", "Venus", "Mercury", "Mars"],
     correctAnswer: "Mercury"
   },
   {
-    question: "A magnet would most likely attract which of the following?", 
+    question: "4) A magnet would most likely attract which of the following?", 
     answers: ["Metal", "Plastic", "Burrito", "Mars"],
     correctAnswer: "Metal"
   },
   {
-    question: "Which planet is closest to Earth?", 
+    question: "5) Which planet is closest to Mars?", 
     answers: ["Earth", "Venus", "Mercury", "Mars"],
-    correctAnswer: "Mercury"
+    correctAnswer: "Mars"
   },
   {
-    question: "Who made the iphone", 
+    question: "6) Who made the Iphone", 
     answers: ["Steve Jobs", "Jayla Ye", "Steve Trabajo", "Kieran Watt"],
     correctAnswer: "Steve Jobs"
   },
   
   {
-    question: "Who made the Pineapple Iphone", 
+    question: "7) Who made the Pineapple Iphone", 
     answers: ["Steve Jobs", "Jayla Ye", "Steve Trabajo", "Kieran Watt"],
     correctAnswer: "Steve Trabajo"
   },
 
 
   {
-    question: "A geologist would likely be LEAST helpful for answering questions about which of the following?", 
+    question: "8) A geologist would likely be LEAST helpful for answering questions about which of the following?", 
     answers: ["Rocks", "Diamonds", "Granite", "Fruity Pebbles"],
     correctAnswer: "Fruity Pebbles"
   },
 
   {
-    question: "A person who is not a banker and lends money at an extremely high interest rate is known as what?", 
+    question: "9) A person who is not a banker and lends money at an extremely high interest rate is known as what?", 
     answers: ["Loan Shark", "Green Snake", "Paper Tiger", "Loan Eagles"],
     correctAnswer: "Loan Shark"
   },
 
   {
-    question: "According to studies, what should a typical teenager spend up to 10 hours a night doing?", 
+    question: "10) According to studies, what should a typical teenager spend up to 10 hours a night doing?", 
     answers: ["Party", "Tweeting", "Surfing", "Sleeping"],
     correctAnswer: "Sleeping"
   },
 
+  {
+    question: "11) In the UK, the abbreviation NHS stands for National what Service?", 
+    answers: ["Humanities", "Health", "Food", "Honor"],
+    correctAnswer: "Health"
+  },
+
+  {
+    question: "12) What is the first pokemon in the pokedex?", 
+    answers: ["Charmander", "Bulbasaur", "Tepig", "Squirtle"],
+    correctAnswer: "Charmander"
+  },
+
+  {
+    question: "13)The hammer and sickle is one of the most recognisable symbols of which political ideology?", 
+    answers: ["Facism", "Communism", "Republicanism", "Liberalism"],
+    correctAnswer: "Communism"
+  },
+
+  {
+    question: "14) In Doctor Who, what was the signature look of the fourth Doctor, as portrayed by Tom Baker?", 
+    answers: ["Bow-tie, braces and tweed jacket", "Wide-brimmed hat and extra long scarf", "Pinstripe suit and trainers", "Cape velvet jacket and frilly shirt"],
+    correctAnswer: "Wide-brimmed hat and extra long scarf"
+  },
+
+  {
+    question: "15) Construction of which of these famous landmarks was completed first?", 
+    answers: ["Empire State Building", "Royal Albert Hall", "Eiffel Tower", "Big Ben Clock Tower"],
+    correctAnswer: "Big Ben Clock Tower"
+  },
+
+  {
+    question: "16) In 1718, which pirate died in battle off the coast of what is now North Carolina?", 
+    answers: ["Calico Jack", "Blackbeard", "Bartholomew Roberts", "Captain Kidd"],
+    correctAnswer: "Blackbeard"
+  },
+
+  {
+    question: "17) Who is the only British politician to have held all four “Great Offices of State” at some point during their career?", 
+    answers: ["David Lloyd George", "Harold Wilson", "James Callaghan", "John Major"],
+    correctAnswer: "James Callaghan"
+  },
+
+  {
+    question: "18) Which of these brands was chiefly associated with the manufacture of household locks?", 
+    answers: ["Phillips", "Flymo", "Chubb", "Ronseal"],
+    correctAnswer: "Chubb"
+  },
+
+  {
+    question: "19) Obstetrics is a branch of medicine particularly concerned with what?", 
+    answers: ["Childbirth", "Broken bones", "Heart conditions", "Old age"],
+    correctAnswer: "Childbirth"
+  },
+
+  {
+    question: "20) Where do you live?", 
+    answers: ["Your house", "Next to your neighbor", "On my street", "Staten Island Technical High School"],
+    correctAnswer: "Staten Island Technical High School"
+  },
 ];
 
 export {questions};
